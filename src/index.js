cccf0c7f6265638d0817e4681dd91d09c46c85f5
sera firincioglu <serafirincioglu@gmail.com>
2022-12-24 18:44:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/HomePage';
+import NavigationBar from './components/GlobalComponents/NavigationBar';
+import Footer from './components/GlobalComponents/Footer';
+import AboutUs from './pages/AboutUs';
+import Products from './pages/Products';
+import Services from './pages/Services';
+import Solutions from './pages/Solutions';
+import News from './pages/News';
+import Career from './pages/Career';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about-us",
+    element: <AboutUs />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
+  },
+  {
+    path: "/solutions",
+    element: <Solutions />,
+  },
+  {
+    path: "/news",
+    element: <News />,
+  },
+  {
+    path: "/career",
+    element: <Career />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavigationBar />
+    <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
